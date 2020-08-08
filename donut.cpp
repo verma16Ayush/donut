e59b2904f4811b1705f79d124d474e6c8f22b570
Ayush <verma16.ayush@gmail.com>
2020-08-09 00:25:12 +0530

    can render the basic circle atleast. kewl.

--- donut.cpp
+++ donut.cpp
@@ -1,14 +1,22 @@
 //giving one last proper shot at donut code.
-//first lets get the basic circle rendered. will add more stuff later as 
+//first lets get the basic circle rendered. will add more stuff later as
 //the work progresses
 
 #include <iostream>
 #include <bits/stdc++.h>
-#include <stdlib.h>
+#include <cstdlib>
 #include <windows.h>
+#include <cmath>
+// #define twoPi 6.28;
 using namespace std;
 
-float del_theta = 0.07; //control theta change. will add more functionality later.
+float delTheta = 0.07; //control theta change. will add more functionality later.
+float r1 = 1, r2 = 2;
+float circleX, circleY;
+const int screenHeight = 40, screenWidth = 80;
+float twoPi = 2 * 3.14;
+// char screen[screenHeight][screenWidth];
+vector< vector<char> > screen(screenHeight, vector<char>(screenWidth, ' '));
 
 void HideCursor()
 {
@@ -19,14 +27,47 @@ void HideCursor()
     SetConsoleCursorInfo(consoleHandle, &cursorInfo);
 }
 
-void render()
+void Render()
 {
+    for (int i = 0; i < screenHeight; i++)
+    {
+        for (int j = 0; j < screenWidth; j++)
+        {
+            cout << screen[i][j];
+        }
+        cout << endl;
+    }
 
+    system("cls");
+}
+
+void Calculate()
+{
+    while (true)
+    {
+        for (float theta = 0; theta < twoPi; theta += delTheta)
+        {
+            circleX = r1 + r2 * cos(theta);
+            circleY = r2 * sin(theta);
+
+            int xp = screenWidth / 2 + circleX;
+            int yp = screenHeight / 2 - circleY;
+
+            if(xp <= screenWidth && yp <= screenHeight)
+            {
+                screen[yp][xp] = '.';
+            }
+            // screen[yp][xp] = '.';
+            Render();
+        }
+    }
 }
 
 int main()
 {
     HideCursor();
-    render();
+    Calculate();
+    // screen[2][2] = 'P';
+    // Render();
     return 0;
 }
